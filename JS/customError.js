c3b8fbe05bbcf9e60204be58861f96da6f562a80
M Wildan F <wildan.fhd@gmail.com>
2022-06-08 11:34:41 +0700

    error handling dan quiz error handling

---
+++ JS/customError.js
@@ -0,0 +1,34 @@
+// Membuat Custom Error dengan class
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError"; // this.name mengambil referensi dari properti error.name
+    }
+}
+
+let json = '{"type":"FPS"}';
+
+try {
+    let newGame = JSON.parse(json);
+
+    if(!newGame.name) {
+        throw new ValidationError("The Game must have a 'name'!");
+    }
+
+    if(!newGame.type) {
+        throw new ValidationError("The Game must have the 'type' of game!");
+    }
+
+    console.log(newGame.game);
+    console.log(newGame.type);
+} catch (error) {
+    if(error instanceof SyntaxError) {
+        console.log(`JSON Error : ${error.message}`);
+    } else if(error instanceof ValidationError) {
+        console.log(`Invalid data : ${error.message} `);
+    } else if(error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
+}
--- JS/errorHandling.js
+++ JS/errorHandling.js
@@ -27,3 +27,48 @@ try {
 
 
 // Throwing Errors
+// Steps : try -> if statement to throw Errors -> going to catch block
+// let json = '{ "brand":"BMW", "type":"SUV"}';
+
+let json = '{"type":"SUV"}'
+
+try {
+    let car = JSON.parse(json); // Mengkonversi json yang berbentuk string menjadi object
+
+    // Throw Error
+    if(!car.brand) {
+        throw new SyntaxError("'brand' is required.");
+    }
+
+    console.log(car.brand); // undefined
+    console.log(car.type); // SUV
+
+} catch(error) {
+    console.log(`JSON Error : ${error.message}`);
+}
+
+// Menampilkan pesan error sesuai error yang muncul
+// Menggunakan instanceOf
+
+let json2 = '{ "name": "Obi Wan", "age": 20 }';
+
+try {
+    let user = JSON.parse(json2);
+
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+
+    errorCode;
+
+    console.log(user.name); // Obi Wan
+    console.log(user.age);  // 20
+} catch (error) {
+    if(error instanceof SyntaxError) {
+        console.log(`JSON Error: ${error.message}`);
+    } else if(error instanceof ReferenceError) {
+        console.log(error.message);
+    } else {
+        console.log(error.stack);
+    }
+}
