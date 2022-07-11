4434613ef49537cb5aceaa3e5d0bc5a014691fa4
M Wildan F <wildan.fhd@gmail.com>
2022-07-11 21:29:20 +0700

    some freecodecamp test

--- dump/test.js
+++ dump/test.js
@@ -26,3 +26,51 @@ function multiplyAll(arr) {
 
   console.log(sum([1, 3, 5], 3));
   
+
+// QUIZ
+
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+
+function lookUpProfile(name, prop) {
+  for (let x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+  console.log(lookUpProfile("Kristian", "lastName"));
+  console.log(lookUpProfile("Sherlock", "likes"));
+  console.log(lookUpProfile("Harry", "likes"));
+  console.log(lookUpProfile("Bob", "number"));
